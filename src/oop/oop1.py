d2022f8bb001534ef8f1e2127da08316425bcc00
Talent Antonio <antonio.talenty@gmail.com>
2019-05-31 08:13:37 +0100

    oop pass

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,13 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+    
